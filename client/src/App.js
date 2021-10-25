137677ee66f511929608e95cec4c77fcf30dd2e4
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-10-25 22:17:29 +0200

    testing out google maps for event app

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+const center = {
+  lat: 45.165287,
+  lng: 17.7664343,
+};
+
+function MyComponent() {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyDkWz-IqoLGml-yATqWjQtQCZUk5VeTt3Y",
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Latitude is :", position.coords.latitude);
+
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  }, []);
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+}
+
+export default React.memo(MyComponent);
---
+++ client/src/TestComponent.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+/* container style */
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+/* --------------- */
+
+const TestComponent = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyDkWz-IqoLGml-yATqWjQtQCZUk5VeTt3Y",
+  });
+
+  const [center, setCenter] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      let newPosition = {};
+      newPosition.lat = position.coords.latitude;
+      newPosition.lng = position.coords.longitude;
+
+      setCenter(newPosition);
+
+      console.log(newPosition);
+    });
+  }, []);
+
+  return (
+    isLoaded && (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={11}
+      ></GoogleMap>
+    )
+  );
+};
+
+export default TestComponent;
